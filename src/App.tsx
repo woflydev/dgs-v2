14ceb6b496b5a3df2e7a8b041cc99e289270a00a
woflydev <wolfy.coding@gmail.com>
2024-04-26 11:30:54 +1000

    FIX: some pnpm shenanigans

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Gallery from "./components/gallery/Gallery";
 import SideMenu from "./components/sideMenu/SideMenu";
 import About from "./components/about/About";
-import Contact from "./components/contact/Contact";
 
 //Styles
 import "./core-ui/index.css";
@@ -17,7 +16,6 @@ const App: FC = () => {
       <Routes>
         <Route path="/" element={<Gallery />} />
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
