e2ab3a373d972ff58c186dd9d5543adef329aed2
woflydev <wolfy.coding@gmail.com>
2024-04-26 12:01:36 +1000

    Revert "FIX: some pnpm shenanigans"
    This reverts commit 14ceb6b496b5a3df2e7a8b041cc99e289270a00a.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Gallery from "./components/gallery/Gallery";
 import SideMenu from "./components/sideMenu/SideMenu";
 import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
 
 //Styles
 import "./core-ui/index.css";
@@ -16,6 +17,7 @@ const App: FC = () => {
       <Routes>
         <Route path="/" element={<Gallery />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
