d7fd5b992e1055d97560c86c24278e7be510285d
woflydev <wolfy.coding@gmail.com>
2024-04-26 11:03:06 +1000

    FIX: index

--- src/components/gallery/Images.tsx
+++ src/components/gallery/Images.tsx
@@ -5,7 +5,7 @@ const Images = ({ imagesToShow, toggle, setModalImg }: ImagesProp) => {
   return (
     <section className="image-list grid sm:grid-cols-1 md:grid-cols-2 2xl:grid-cols-3  gap-1 mx-auto overflow-hidden w-full">
       {imagesToShow.map((img, index) => (
-        <section className="card-zoom rounded-lg" key={index}>
+        <section className="card-zoom" key={index}>
           <img onClick={() => {toggle(); setModalImg(index)}} src={img.src} alt={img.alt} className="card-zoom-image round-image" />
         </section>
       ))}
